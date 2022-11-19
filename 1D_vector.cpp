29369a0ce85f4f2e98ba20e2c4295af3c507a3b3
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2022-11-19 23:44:26 +0600

    Add files via upload

---
+++ 1D_vector.cpp
@@ -0,0 +1,13 @@
+//Vector declare & push_back & print...
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   vector<int>v;//Vector declare...
+   //Value push_back...
+   v.push_back(10);
+   v.push_back(50);
+   v.push_back(40);
+   v.push_back(30);
+   cout<<v[1]<<endl;//print Vector...
+    return 0;
+}
