9d9449b3395d14037eaff3d3cca56e87e1adaca7
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2022-11-21 00:45:01 +0600

    print all the vector elements by using for each loop

---
+++ for each loop.cpp
@@ -0,0 +1,14 @@
+//print all the vector elements by using for each loop
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int>v={50,60,70,80,90,100};//vector declare and initialize
+    for(int u:v)//for each loop
+    {
+        cout<<u<< " ";//print the value one by one
+    }
+
+    return 0;
+}
