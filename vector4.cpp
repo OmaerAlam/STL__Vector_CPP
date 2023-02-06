ddaa169450e001aed589014fca8ff7723d96c0ed
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-02-06 13:55:23 +0600

    vector size & for loop.

---
+++ vector4.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    cout<<v.size()<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
