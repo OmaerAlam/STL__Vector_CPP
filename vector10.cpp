52ade9a0a37694815caaff4503a27450e4382ce3
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-02-06 14:52:00 +0600

    vector copy.

---
+++ vector10.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v{2,3,4,5};
+    vector<int>tmp;
+    tmp=v;
+    cout<<tmp.size()<<endl;
+    for(int i=0;i<tmp.size();i++) cout<<tmp[i]<<" ";
+    cout<<endl;
+    //If I want to run v.
+    cout<<v.size()<<endl;
+    for(int i=0;i<v.size();i++) cout<<v[i]<<" ";
+    cout<<endl;
+}
