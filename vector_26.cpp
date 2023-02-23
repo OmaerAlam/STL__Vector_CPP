4904d21c85f5c1b70c3bf612983f2cf7bf51aaae
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-02-24 00:16:33 +0600

    Z. Vector Max Element.

---
+++ vector_26.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={2,3,5,5,7,2,10,3,7,1};
+
+       vector<int> :: iterator it=max_element(v.begin(),v.end());
+       cout<<*it<<endl;
+
+return 0;
+}
