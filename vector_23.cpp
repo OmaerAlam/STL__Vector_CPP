781123df592bb646d1f1d15801bc66b191f1e531
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-02-23 23:55:50 +0600

    W. Vector Middle Element Delete.

---
+++ vector_23.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={2,3,5,7,10};
+
+        v.erase(v.begin()+2);
+    cout<<v.size()<<endl;
+    for(auto u:v) cout<<u<<" ";
+    cout<<endl;
+
+return 0;
+}
